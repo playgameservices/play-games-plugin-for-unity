3d87c4968d274579b14a81e454d48f4f631df7f6
Clayton Wilkinson <wilkinsonclay@google.com>
2018-01-22 12:26:57 -0800

    Updating PluginSupportLib to use CMake to build native lib.
    Change-Id: Ib624611da620c53dc207cd364f95662500f76618

--- source/native/Application.mk
+++
@@ -1,7 +0,0 @@
-APP_STL := gnustl_static  # Or c++_shared, gnustl_shared, or gnustl_static
-APP_CPPFLAGS := -std=c++11
-APP_PLATFORM := android-9
-
-ifndef APP_ABI
-  APP_ABI := armeabi-v7a x86
-endif
