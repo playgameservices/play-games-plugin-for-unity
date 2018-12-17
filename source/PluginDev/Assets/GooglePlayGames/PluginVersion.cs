cddb95feff97cdaf4057ce843dd647e220634a32
olehkuznetsov <okuznetsov@google.com>
2018-12-17 15:39:02 +0000

    Updating plugin to 0.9.57

--- source/PluginDev/Assets/GooglePlayGames/PluginVersion.cs
+++ source/PluginDev/Assets/GooglePlayGames/PluginVersion.cs
@@ -34,9 +34,9 @@ namespace GooglePlayGames
     public const string VersionKeyJarResolverDLL = "00935";
 
     // Current Version.
-    public const int VersionInt = 0x0955;
-    public const string VersionString = "0.9.55";
-    public const string VersionKey = "00955";
+    public const int VersionInt = 0x0957;
+    public const string VersionString = "0.9.57";
+    public const string VersionKey = "00957";
 
     // used to check for the correct min version or play services: 10.2
     public const int MinGmsCoreVersionCode = 10200000;
