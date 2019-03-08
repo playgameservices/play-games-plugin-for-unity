e61be0b552d53ab01ed01a391c376b2fa0022125
habitoti <hansjr@web.de>
2019-03-08 22:02:49 +0100

    Expose match dismissal

--- source/PluginDev/Assets/GooglePlayGames/BasicApi/Multiplayer/ITurnBasedMultiplayerClient.cs
+++ source/PluginDev/Assets/GooglePlayGames/BasicApi/Multiplayer/ITurnBasedMultiplayerClient.cs
@@ -197,6 +197,19 @@ namespace GooglePlayGames.BasicApi.Multiplayer
         void Cancel(TurnBasedMatch match, Action<bool> callback);
 
         /// <summary>
+        /// Dismiss a match.
+        /// </summary>
+        /// <remarks>Dismissing a match hides it from the dismisser's match list UI
+        /// and causes the match to eventually expire. Other match participants can continue 
+        /// to play until the dismissed match expires after two weeks, or until the match is 
+        /// played to completion or canceled (whichever happens first). To other participants, 
+        /// the dismisser still appears as a participant in the match. Another player cannot 
+        /// take the dismisser's place.
+        /// </remarks>
+        /// <param name="match">Match identifier.</param>
+        void Dismiss(TurnBasedMatch match);
+
+        /// <summary>
         /// Request a rematch.</summary>
         /// <remarks>
         /// This can be used on a finished match in order to start a new
