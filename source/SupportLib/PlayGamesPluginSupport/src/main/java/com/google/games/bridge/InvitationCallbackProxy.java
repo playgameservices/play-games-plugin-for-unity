ac6682cd2cb9b731fdda12aed477f4ab11fab5bf
olehkuznetsov <okuznetsov@google.com>
2019-03-15 18:13:09 +0000

    Implemented RoomUpdateCallbackProxy

--- source/SupportLib/PlayGamesPluginSupport/src/main/java/com/google/games/bridge/InvitationCallbackProxy.java
+++ source/SupportLib/PlayGamesPluginSupport/src/main/java/com/google/games/bridge/InvitationCallbackProxy.java
@@ -1,26 +1,9 @@
-/*
- * Copyright (C) Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.games.bridge;
 
 import android.support.annotation.NonNull;
 import com.google.android.gms.games.multiplayer.Invitation;
 import com.google.android.gms.games.multiplayer.InvitationCallback;
 
-// can be removed if com.google.android.gms.games.multiplayer.InvitationCallback becomes interface
 public class InvitationCallbackProxy extends InvitationCallback {
     private Callback callback;
 
@@ -37,7 +20,7 @@ public class InvitationCallbackProxy extends InvitationCallback {
     }
 
     public interface Callback {
-        public abstract void onInvitationReceived(@NonNull Invitation invitation);
-        public abstract void onInvitationRemoved(@NonNull String invitationId);        
+        void onInvitationReceived(@NonNull Invitation invitation);
+        void onInvitationRemoved(@NonNull String invitationId);        
     }
 }
