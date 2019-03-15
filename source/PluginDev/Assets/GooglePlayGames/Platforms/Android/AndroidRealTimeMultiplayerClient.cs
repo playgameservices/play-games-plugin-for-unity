72373cb5169dbfee3e587944e5bf0c874500c18a
olehkuznetsov <okuznetsov@google.com>
2019-03-15 18:14:16 +0000

    Implemented CreateQuickGame method and add java mappings

--- source/PluginDev/Assets/GooglePlayGames/Platforms/Android/AndroidRealTimeMultiplayerClient.cs
+++ source/PluginDev/Assets/GooglePlayGames/Platforms/Android/AndroidRealTimeMultiplayerClient.cs
@@ -12,6 +12,8 @@ namespace GooglePlayGames.Android
     internal class AndroidRealTimeMultiplayerClient : IRealTimeMultiplayerClient
     {
         private volatile AndroidJavaObject mClient;
+        private AndroidJavaObject mRoomConfig;
+        private AndroidJavaObject mRoom;
 
         public AndroidRealTimeMultiplayerClient(AndroidJavaObject account) 
         {
@@ -24,49 +26,78 @@ namespace GooglePlayGames.Android
         public void CreateQuickGame(uint minOpponents, uint maxOpponents, uint variant,
                             RealTimeMultiplayerListener listener)
         {
+            CreateQuickGame(minOpponents, maxOpponents, variant, /* exclusiveBitMask= */ 0, listener);
         }
 
         public void CreateQuickGame(uint minOpponents, uint maxOpponents, uint variant,
             ulong exclusiveBitMask,
             RealTimeMultiplayerListener listener)
         {
+            AndroidJavaObject roomUpdateCallback = new AndroidJavaObject("com.google.games.bridge.RoomUpdateCallbackProxy",
+                new RoomUpdateCallbackProxy(/* parent= */this, listener));
+            AndroidJavaObject realTimeListener = null; //listener
+            // build room config
+            using (var roomConfigClass = new AndroidJavaClass("com.google.android.gms.games.multiplayer.realtime.RoomConfig"))
+            {
+                using (var roomConfigBuilder = roomConfigClass.CallStatic<AndroidJavaObject>("builder", roomUpdateCallback))
+                {
+                    roomConfigBuilder.Call<AndroidJavaObject>("setVariant", (int)variant);
+                    roomConfigBuilder.Call<AndroidJavaObject>("setAutoMatchCriteria",
+                        roomConfigBuilder.CallStatic<AndroidJavaObject>("createAutoMatchCriteria", (int)minOpponents, (int)maxOpponents, (long)exclusiveBitMask));
+                    mRoomConfig = roomConfigBuilder.Call<AndroidJavaObject>("build");
+                }
+            }
+            using (var task = mClient.Call<AndroidJavaObject>("create", mRoomConfig))
+            {
+            }
         }
 
         public void CreateWithInvitationScreen(uint minOpponents, uint maxOppponents, uint variant,
                                         RealTimeMultiplayerListener listener)
         {
+            // Task<Intent> getSelectOpponentsIntent(@IntRange(from = 1) int minPlayers, @IntRange(from = 1) int maxPlayers, boolean allowAutomatch)
         }
 
         public void ShowWaitingRoomUI()
         {
+            // Task<Intent> getWaitingRoomIntent(@NonNull Room room, @IntRange(from = 0) int minParticipantsToStart)
         }
 
         public void GetAllInvitations(Action<Invitation[]> callback)
         {
+            // Task<AnnotatedData<InvitationBuffer>> InvitationsClient.loadInvitations()
         }
 
         public void AcceptFromInbox(RealTimeMultiplayerListener listener)
         {
+            // Task<Intent> getInvitationInboxIntent()
         }
 
         public void AcceptInvitation(string invitationId, RealTimeMultiplayerListener listener)
         {
+            // Task<Void> join(@NonNull RoomConfig config)
         }
 
         public void SendMessageToAll(bool reliable, byte[] data)
         {
+            // Task<Void> sendUnreliableMessageToOthers(@NonNull byte[] messageData, @NonNull String roomId)
         }
 
         public void SendMessageToAll(bool reliable, byte[] data, int offset, int length)
         {
+            // Task<Void> sendUnreliableMessageToOthers(@NonNull byte[] messageData, @NonNull String roomId)
         }
 
         public void SendMessage(bool reliable, string participantId, byte[] data)
         {
+            // Task<Integer> sendReliableMessage(@NonNull byte[] messageData, @NonNull String roomId, @NonNull String recipientParticipantId, @Nullable ReliableMessageSentCallback callback)
+            // Task<Void> sendUnreliableMessage(@NonNull byte[] messageData, @NonNull String roomId, @NonNull String recipientParticipantId)
         }
 
         public void SendMessage(bool reliable, string participantId, byte[] data, int offset, int length)
         {
+            // Task<Integer> sendReliableMessage(@NonNull byte[] messageData, @NonNull String roomId, @NonNull String recipientParticipantId, @Nullable ReliableMessageSentCallback callback)
+            // Task<Void> sendUnreliableMessage(@NonNull byte[] messageData, @NonNull String roomId, @NonNull String recipientParticipantId)
         }
 
         public List<Participant> GetConnectedParticipants()
@@ -91,6 +122,7 @@ namespace GooglePlayGames.Android
 
         public void LeaveRoom()
         {
+            // Task<Void> leave(@NonNull RoomConfig config, @NonNull String roomId)
         }
 
         public bool IsRoomConnected()
@@ -100,6 +132,40 @@ namespace GooglePlayGames.Android
 
         public void DeclineInvitation(string invitationId)
         {
+            // Task<Void> declineInvitation(@NonNull String invitationId)
+        }
+
+        private class RoomUpdateCallbackProxy : AndroidJavaProxy
+        {
+            private RealTimeMultiplayerListener mListener;
+            private AndroidRealTimeMultiplayerClient mParent;
+            
+            public RoomUpdateCallbackProxy(AndroidRealTimeMultiplayerClient parent, RealTimeMultiplayerListener listener) 
+                : base("com/google/games/bridge/RoomUpdateCallbackProxy$Callback")
+            {
+                mListener = listener;
+                mParent = parent;
+            }
+
+            public void onRoomCreated(/* @OnRoomCreatedStatusCodes */ int statusCode, /* @Nullable Room */ AndroidJavaObject room)
+            {
+                mParent.mRoom = room;
+            }
+
+            public void onJoinedRoom(/* @OnJoinedRoomStatusCodes */ int statusCode, /* @Nullable Room */ AndroidJavaObject room)
+            {
+                mParent.mRoom = room;
+            }
+
+            public void onLeftRoom(/* @OnLeftRoomStatusCodes */ int statusCode, /* @Nullable */ string roomId)
+            {
+                mListener.OnLeftRoom();
+            }
+
+            public void onRoomConnected(/* @OnRoomConnectedStatusCodes */ int statusCode, /* @Nullable Room */ AndroidJavaObject room)
+            {
+                mListener.OnRoomConnected(true);
+            }
         }
     }
 }
