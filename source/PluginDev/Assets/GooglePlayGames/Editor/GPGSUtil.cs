77a28ec0519c71b5eda38278d8b0f48cc07f9d59
bur <egor.kulagin@tortugasocial.com>
2019-05-28 15:22:56 +0300

    fix(GPGSUtil): fix detecting AndroidSdkRoot if used unity embedded

--- source/PluginDev/Assets/GooglePlayGames/Editor/GPGSUtil.cs
+++ source/PluginDev/Assets/GooglePlayGames/Editor/GPGSUtil.cs
@@ -449,11 +449,15 @@ namespace GooglePlayGames.Editor
         public static string GetAndroidSdkPath()
         {
             string sdkPath = EditorPrefs.GetString("AndroidSdkRoot");
-            if (sdkPath != null && (sdkPath.EndsWith("/") || sdkPath.EndsWith("\\")))
-            {
-                sdkPath = sdkPath.Substring(0, sdkPath.Length - 1);
+            // Unity 2019.x added installation of the Android SDK in the AndroidPlayer directory
+            // so fallback to searching for it there.
+            if (String.IsNullOrEmpty(sdkPath) || EditorPrefs.GetBool("SdkUseEmbedded")) {
+                string androidPlayerDir = BuildPipeline.GetPlaybackEngineDirectory(BuildTarget.Android, BuildOptions.None);
+            if (!String.IsNullOrEmpty(androidPlayerDir)) {
+                    string androidPlayerSdkDir = Path.Combine(androidPlayerDir, "SDK");
+                    if (Directory.Exists(androidPlayerSdkDir)) sdkPath = androidPlayerSdkDir;
+                }
             }
-
             return sdkPath;
         }
 
