1a1dc88b466d291068f05cd9bce70383b8157ea6
Mehmet <mehmetozdemir@google.com>
2019-11-06 18:02:26 +0000

    Release v10.06

--- source/PluginDev/Assets/GooglePlayGames/PluginVersion.cs
+++ source/PluginDev/Assets/GooglePlayGames/PluginVersion.cs
@@ -19,8 +19,8 @@ namespace GooglePlayGames
     public class PluginVersion
     {
         // Current Version.
-        public const int VersionInt = 0x01005;
-        public const string VersionString = "0.10.05";
-        public const string VersionKey = "01005";
+        public const int VersionInt = 0x01006;
+        public const string VersionString = "0.10.06";
+        public const string VersionKey = "01006";
     }
 }
