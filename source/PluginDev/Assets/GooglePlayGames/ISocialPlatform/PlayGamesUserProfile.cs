38138481a25246d75636040278a5d98fef9ca135
Caroline Yates <cya@google.com>
2020-01-06 09:05:45 -0800

    Internal sync
    PiperOrigin-RevId: 288311397
    Change-Id: Ib974c196b9726ed2afb4f4bb65bd156520a32c6d

--- source/PluginDev/Assets/GooglePlayGames/ISocialPlatform/PlayGamesUserProfile.cs
+++ source/PluginDev/Assets/GooglePlayGames/ISocialPlatform/PlayGamesUserProfile.cs
@@ -22,7 +22,7 @@ namespace GooglePlayGames
     using System.Collections;
     using GooglePlayGames.OurUtils;
     using UnityEngine;
-#if UNITY_2017_1_OR_NEWER
+#if UNITY_2017_2_OR_NEWER
     using UnityEngine.Networking;
 #endif
     using UnityEngine.SocialPlatforms;
@@ -120,7 +120,7 @@ namespace GooglePlayGames
             // avatar configured.
             if (!string.IsNullOrEmpty(AvatarURL))
             {
-#if UNITY_2017_1_OR_NEWER
+#if UNITY_2017_2_OR_NEWER
                 UnityWebRequest www = UnityWebRequestTexture.GetTexture(AvatarURL);
                 www.SendWebRequest();
 #else
@@ -133,7 +133,7 @@ namespace GooglePlayGames
 
                 if (www.error == null)
                 {
-#if UNITY_2017_1_OR_NEWER
+#if UNITY_2017_2_OR_NEWER
                     this.mImage = DownloadHandlerTexture.GetContent(www);
 #else
                     this.mImage = www.texture;
