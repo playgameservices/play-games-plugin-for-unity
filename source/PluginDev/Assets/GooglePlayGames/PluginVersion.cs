78e2ba7d702f702c9adac0c573e9a18c06363647
Play Games Services Team <play-games-services-mail+github@google.com>
2020-11-18 02:45:16 -0800

    v10.12 release.
    PiperOrigin-RevId: 343043314
    Change-Id: Ic49e2d80f90666594e4fadbe7f99672168122821

--- source/PluginDev/Assets/GooglePlayGames/PluginVersion.cs
+++ source/PluginDev/Assets/GooglePlayGames/PluginVersion.cs
@@ -19,8 +19,8 @@ namespace GooglePlayGames
     public class PluginVersion
     {
         // Current Version.
-        public const int VersionInt = 0x01011;
-        public const string VersionString = "0.10.11";
-        public const string VersionKey = "01011" ;
+        public const int VersionInt = 0x01012;
+        public const string VersionString = "0.10.12";
+        public const string VersionKey = "01012" ;
     }
 }
