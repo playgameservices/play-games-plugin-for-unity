28672eb7ec73f581211e6e56f5b4577a577ab65d
Mehmet Özdemir <mehmetozdemir@google.com>
2021-11-18 08:01:00 -0800

    Fix #2876.
    PiperOrigin-RevId: 410804543
    Change-Id: Ia970e5a94399472f1650eb6d03bfe6f9771b84c4

--- source/PluginDev/Assets/GooglePlayGames/ISocialPlatform/PlayGamesLeaderboard.cs
+++ source/PluginDev/Assets/GooglePlayGames/ISocialPlatform/PlayGamesLeaderboard.cs
@@ -157,11 +157,10 @@ namespace GooglePlayGames
                 {
                     if (fid.Equals(score.userID))
                     {
-                        return mScoreList.Count;
+                        mScoreList.Add(score);
+                        break;
                     }
                 }
-
-                mScoreList.Add(score);
             }
 
             return mScoreList.Count;
