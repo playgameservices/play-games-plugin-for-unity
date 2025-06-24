bf926fc50c17b692144861bd6757cafa7b20eae5
Play Games Services Team <play-games-services-mail+github@google.com>
2025-06-24 02:16:44 -0700

    Add Documentation for AdvertisingResult.cs
    PiperOrigin-RevId: 775137260
    Change-Id: I81afd5e6237501a595c3d98b1662e397de777883

--- Assets/Public/GooglePlayGames/com.google.play.games/Runtime/Scripts/BasicApi/Nearby/AdvertisingResult.cs
+++ Assets/Public/GooglePlayGames/com.google.play.games/Runtime/Scripts/BasicApi/Nearby/AdvertisingResult.cs
@@ -20,27 +20,45 @@ namespace GooglePlayGames.BasicApi.Nearby
     using System.Collections.Generic;
     using GooglePlayGames.OurUtils;
 
+    /// <summary>
+    /// Represents the result of an attempt to start advertising for nearby connections.
+    /// </summary>
     public struct AdvertisingResult
     {
         private readonly ResponseStatus mStatus;
         private readonly string mLocalEndpointName;
 
+        /// <summary>
+        /// Constructs a new <see cref="AdvertisingResult"/>.
+        /// </summary>
+        /// <param name="status">The result of the advertising attempt.</param>
+        /// <param name="localEndpointName">The name of the local endpoint.</param>
+        /// <exception cref="System.ArgumentNullException">If <see cref="localEndpointName"/> is null.</exception>
         public AdvertisingResult(ResponseStatus status, string localEndpointName)
         {
             this.mStatus = status;
             this.mLocalEndpointName = Misc.CheckNotNull(localEndpointName);
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the advertising operation was successful.
+        /// </summary>
         public bool Succeeded
         {
             get { return mStatus == ResponseStatus.Success; }
         }
 
+        /// <summary>
+        /// Gets the response status of the advertising operation.
+        /// </summary>
         public ResponseStatus Status
         {
             get { return mStatus; }
         }
 
+        /// <summary>
+        /// Gets the name of the local endpoint used in the advertising operation.
+        /// </summary>
         public string LocalEndpointName
         {
             get { return mLocalEndpointName; }
