2da519fc2ba893c52a8f79418eb348fcd198f102
Play Games Services Team <play-games-services-mail+github@google.com>
2025-06-23 22:54:35 -0700

    Add Documentation for EndpointDetails.cs
    PiperOrigin-RevId: 775076804
    Change-Id: I4e66cb703ea8bba9a182e7224390abff8875f9c3

--- Assets/Public/GooglePlayGames/com.google.play.games/Runtime/Scripts/BasicApi/Nearby/EndpointDetails.cs
+++ Assets/Public/GooglePlayGames/com.google.play.games/Runtime/Scripts/BasicApi/Nearby/EndpointDetails.cs
@@ -18,12 +18,21 @@ namespace GooglePlayGames.BasicApi.Nearby
 {
     using GooglePlayGames.OurUtils;
 
+    /// <summary>
+    /// Represents details of an endpoint involved in a Nearby Connections operation.
+    /// </summary>
     public struct EndpointDetails
     {
         private readonly string mEndpointId;
         private readonly string mName;
         private readonly string mServiceId;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EndpointDetails"/> struct.
+        /// </summary>
+        /// <param name="endpointId">The unique identifier of the endpoint.</param>
+        /// <param name="name">The name of the endpoint.</param>
+        /// <param name="serviceId">The service ID associated with the endpoint.</param>
         public EndpointDetails(string endpointId, string name, string serviceId)
         {
             this.mEndpointId = Misc.CheckNotNull(endpointId);
@@ -31,16 +40,25 @@ namespace GooglePlayGames.BasicApi.Nearby
             this.mServiceId = Misc.CheckNotNull(serviceId);
         }
 
+        /// <summary>
+        /// Gets the unique identifier of the endpoint.
+        /// </summary>
         public string EndpointId
         {
             get { return mEndpointId; }
         }
 
+        /// <summary>
+        /// Gets the name of the endpoint.
+        /// </summary>
         public string Name
         {
             get { return mName; }
         }
 
+        /// <summary>
+        /// Gets the service ID associated with the endpoint.
+        /// </summary>
         public string ServiceId
         {
             get { return mServiceId; }
