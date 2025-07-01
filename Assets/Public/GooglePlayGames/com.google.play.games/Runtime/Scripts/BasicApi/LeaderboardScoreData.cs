d877befa913ef7818c6df82c7e7aed27182d85ea
Play Games Services Team <play-games-services-mail+github@google.com>
2025-07-01 02:08:31 -0700

    Add summary tags for LeaderboardScoreData.cs
    PiperOrigin-RevId: 777936645
    Change-Id: I35c10b49e2c66f268a7f8cf1c140464669eb490e

--- Assets/Public/GooglePlayGames/com.google.play.games/Runtime/Scripts/BasicApi/LeaderboardScoreData.cs
+++ Assets/Public/GooglePlayGames/com.google.play.games/Runtime/Scripts/BasicApi/LeaderboardScoreData.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="LeaderboardScoreData.cs" company="Google Inc.">
+// <copyright file="LeaderboardScoreData.cs" company="Google Inc.">
 // Copyright (C) 2015 Google Inc. All Rights Reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -38,17 +38,29 @@ namespace GooglePlayGames.BasicApi
         private ScorePageToken mNextPage;
         private List<PlayGamesScore> mScores = new List<PlayGamesScore>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LeaderboardScoreData"/> class.
+        /// </summary>
+        /// <param name="leaderboardId">The identifier of the leaderboard.</param>
         internal LeaderboardScoreData(string leaderboardId)
         {
             mId = leaderboardId;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LeaderboardScoreData"/> class with a specified status.
+        /// </summary>
+        /// <param name="leaderboardId">The identifier of the leaderboard.</param>
+        /// <param name="status">The response status of the leaderboard data.</param>
         internal LeaderboardScoreData(string leaderboardId, ResponseStatus status)
         {
             mId = leaderboardId;
             mStatus = status;
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the leaderboard data is valid.
+        /// </summary>
         public bool Valid
         {
             get
@@ -58,6 +70,9 @@ namespace GooglePlayGames.BasicApi
             }
         }
 
+        /// <summary>
+        /// Gets or sets the status of the leaderboard data response.
+        /// </summary>
         public ResponseStatus Status
         {
             get { return mStatus; }
@@ -65,6 +80,9 @@ namespace GooglePlayGames.BasicApi
             internal set { mStatus = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the approximate count of scores in the leaderboard.
+        /// </summary>
         public ulong ApproximateCount
         {
             get { return mApproxCount; }
@@ -72,6 +90,9 @@ namespace GooglePlayGames.BasicApi
             internal set { mApproxCount = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the title of the leaderboard.
+        /// </summary>
         public string Title
         {
             get { return mTitle; }
@@ -79,6 +100,9 @@ namespace GooglePlayGames.BasicApi
             internal set { mTitle = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the unique identifier of the leaderboard.
+        /// </summary>
         public string Id
         {
             get { return mId; }
@@ -86,6 +110,9 @@ namespace GooglePlayGames.BasicApi
             internal set { mId = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the player's score in the leaderboard.
+        /// </summary>
         public IScore PlayerScore
         {
             get { return mPlayerScore; }
@@ -93,17 +120,28 @@ namespace GooglePlayGames.BasicApi
             internal set { mPlayerScore = value; }
         }
 
+        /// <summary>
+        /// Gets an array of the scores in the leaderboard.
+        /// </summary>
         public IScore[] Scores
         {
             get { return mScores.ToArray(); }
         }
 
+        /// <summary>
+        /// Adds a score to the leaderboard data.
+        /// </summary>
+        /// <param name="score">The score to add.</param>
+        /// <returns>The count of scores after the addition.</returns>
         internal int AddScore(PlayGamesScore score)
         {
             mScores.Add(score);
             return mScores.Count;
         }
 
+        /// <summary>
+        /// Gets or sets the token for the previous page of scores.
+        /// </summary>
         public ScorePageToken PrevPageToken
         {
             get { return mPrevPage; }
@@ -111,6 +149,9 @@ namespace GooglePlayGames.BasicApi
             internal set { mPrevPage = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the token for the next page of scores.
+        /// </summary>
         public ScorePageToken NextPageToken
         {
             get { return mNextPage; }
@@ -118,6 +159,10 @@ namespace GooglePlayGames.BasicApi
             internal set { mNextPage = value; }
         }
 
+        /// <summary>
+        /// Returns a string representation of the leaderboard score data.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
         public override string ToString()
         {
             return string.Format("[LeaderboardScoreData: mId={0}, " +
